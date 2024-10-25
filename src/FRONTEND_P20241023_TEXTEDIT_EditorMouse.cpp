c1a8d0fe9784b8cce726717f3fe4e5b93244d761
MarkusHWeisse <147412030+MarkusHWeisse@users.noreply.github.com>
2024-10-25 08:34:44 +0200

    create EditorMouse.cpp

---
+++ src/FRONTEND_P20241023_TEXTEDIT_EditorMouse.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <iterator>
+#include <windows.h>
+#include <shobjidl.h>
+#include <objbase.h>
+
+#include "FRONTEND_P20241023_TEXTEDIT.h"
+#include "FRONTEND_P20241023_TEXTEDIT_EditorMouse.h"
+#include "FRONTEND_P20241023_TEXTEDIT_Cursor.h"
+#include "FRONTEND_P20241023_TEXTEDIT_Slider.h"
+#include "FRONTEND_P20241023_TEXTEDIT_Text.h"*/
+#include "FRONTEND_P20241023_TEXTEDIT_MarkedText.h"
+#include "Editor.h"
+
+//Bad Design: intention not obvious -> change!
+void FRONTEND_P20241023_TEXTEDIT::EditorMouse::loadMouseCoordinates(sf::RenderWindow &window) {
+	mcords = m.getPosition(window);
+}
+
+
+int FRONTEND_P20241023_TEXTEDIT::EditorMouse::getPosX() {
+	return mcords.x;
+}
+
+int FRONTEND_P20241023_TEXTEDIT::EditorMouse::getPosY() {
+	return mcords.y;
+}
+
+sf::Vector2i& FRONTEND_P20241023_TEXTEDIT::EditorMouse::getMouseCords() {
+	return mcords;
+}
+
+void FRONTEND_P20241023_TEXTEDIT::EditorMouse::setInUse(bool t) {
+	inUse = t;
+}
+
+bool FRONTEND_P20241023_TEXTEDIT::EditorMouse::getInUse() {
+	return inUse;
+}
